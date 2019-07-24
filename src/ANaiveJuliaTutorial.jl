1afdc307600d3c658f6b27cf0099792c89fc2978
Qi Zhang <singularitti@outlook.com>
2019-07-24 14:42:10 -0400

    Update src
    Referenced from https://github.com/Roger-luo/TutorialZH.jl/blob/master/src/TutorialZH.jl

--- src/ANaiveJuliaTutorial.jl
+++ src/ANaiveJuliaTutorial.jl
@@ -1,5 +1,34 @@
 module ANaiveJuliaTutorial
 
-greet() = print("Hello World!")
+using IJulia, Pkg
+
+export tutorial, notebooks
+
+function tutorial()
+    cmd = IJulia.find_jupyter_subcommand("notebook")
+    push!(cmd.exec, joinpath(@__DIR__, "..", "notebooks", "tutorial.ipynb"))
+    return IJulia.launch(cmd, joinpath(@__DIR__, "..", "notebooks"), false)
+end
+
+function notebooks()
+    return IJulia.notebook(dir=joinpath(@__DIR__, "..", "notebooks"))
+end
+
+REQUIRE = [
+    "PyCall",
+    "IJulia",
+    "Revise",
+    "Interact",
+]
+
+function __init__()
+    for each in REQUIRE
+        if each in keys(Pkg.installed())
+            continue
+        else
+            Pkg.add(each)
+        end
+    end
+end
 
 end # module
